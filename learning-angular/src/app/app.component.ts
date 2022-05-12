f8512e4f4d9ab9010dc51d35bba4717042536560
Alex Millerman <amillerman@users.noreply.github.com>
2022-05-12 16:09:47 -0400

    Created our first angular application and updated the app component with some one way binding

---
+++ learning-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '<h1>New heading!</h1>';
+}
