87b746058bea928d7f9a22d121f060486fc851f0
Alex Millerman <amillerman@users.noreply.github.com>
2022-05-12 16:29:14 -0400

    Migrated the stylesheets over to SCSS
    Installed scss migrate package:
    npm install schematics-scss-migrate
    
    Ran this in the console afterwards:
    npm run-script ng g schematics-scss-migrate:scssmigrate

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = '<h1>New heading!</h1>';
