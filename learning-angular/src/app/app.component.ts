1cd7b8092191c10974067d369b63e60640ec4c66
Alex Millerman <amillerman01@stclaircollege.ca>
2022-05-13 14:59:43 -0400

    Populated the content interface and made an instance of it

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Content } from "./content";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,8 @@ export class AppComponent {
   title = '<h1>New heading!</h1>';
   name: string; // this variable will now always be a string
   tertiaryName = 'Alex'; // this variable will ALSO now always be a string
+
+  individualContentItem: Content;
 
   constructor() {
 
@@ -21,6 +24,14 @@ export class AppComponent {
     // this.tertiaryName = 1;
 
     this.updateName();
+
+    this.individualContentItem = {
+      id: 0,
+      type: "Platformer",
+      imageUrl: "https://assets.nintendo.com/image/upload/c_scale,f_auto,q_auto,w_1280/v1/ncom/en_US/games/switch/s/super-mario-odyssey-switch/Video/posters/Super_Mario_Odyssey_Accolades_Trailer",
+      body: "Very good platformer on the Nintendo Switch",
+      name: "Mario Odyssey"
+    };
   }
 
   updateName(): void{
--- learning-angular/src/app/content.ts
+++ learning-angular/src/app/content.ts
@@ -1,2 +1,7 @@
 export interface Content {
+  id: number;
+  name: string;
+  type: string;
+  imageUrl: string;
+  body: string;
 }
