69e5572ab2c9b49b7313541d8eeaa05478bbfd2e
Alex Millerman <amillerman01@stclaircollege.ca>
2022-05-13 14:29:50 -0400

    Added examples of variable scope and implicit types being set on variables

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -7,11 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '<h1>New heading!</h1>';
-  name: string;
+  name: string; // this variable will now always be a string
+  tertiaryName = 'Alex'; // this variable will ALSO now always be a string
 
   constructor() {
 
     this.name = 'Peter';
 
+    let secondaryName: string;
+    secondaryName = 'Andrew';
+
+    // can't do this becasue tertiaryName is a string type
+    // this.tertiaryName = 1;
   }
 }
