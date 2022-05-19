9f4dc949fb2917761b820a4261f6dbcad69aec8d
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-19 16:33:59 -0400

    Converted the content into an array and displayed it with *ngFor

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -8,25 +8,35 @@ import { ContentList } from "./content-list";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  individualItem: Content;
-  individualItem2: Content;
+  chessPlayers: Content[];
 
   constructor() {
 
-    this.individualItem = {
+    this.chessPlayers = [{
       id: 0,
       name: 'Magnus Carlsen',
       body: "Current world champ",
       imageUrl: "https://i.guim.co.uk/img/media/ad35976aca03db7b6b355111a3dd4e3db687d724/0_136_2048_1229/master/2048.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=c2b38f880baf6f2edba26b98fdf4420c",
       type: "GM"
-    };
-    this.individualItem2 = {
+    }, {
       id: 1,
       name: 'Alexandra Botez',
       body: "	Woman FIDE Master",
       imageUrl: "https://images.chesscomfiles.com/uploads/v1/master_player/7064c138-1af7-11eb-b69b-0371950d641f.8e25e027.5000x5000o.6254ebef0621.jpeg",
       type: "FM"
-    }
+    }, {
+      id: 2,
+      name: 'Rameshbabu Praggnanandhaa',
+      body: "Grandmaster",
+      imageUrl: "https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F220222035049-01-praggnanandhaa-chess-portrait-file-2019.jpg",
+      type: "GM"
+    }, {
+      id: 3,
+      name: 'Ben Finegold',
+      body: "Fide Grandmaster",
+      imageUrl: "https://chessdailynews.com/wp-content/uploads/2010/01/Ben-Finegold-4.jpg",
+      type: "GM"
+    }];
 
   }
 }
