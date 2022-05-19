5238eaedefdee04821cc1382e3a3f6fdaea497cf
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-19 16:01:43 -0400

    Added the content card to the app component and added some styles

--- learning-angular/src/app/content-card/content-card.component.ts
+++ learning-angular/src/app/content-card/content-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from '../content';
 
 @Component({
   selector: 'app-content-card',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
+  contentItem?: Content;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.contentItem = {
+      id: 0,
+      name: 'Magnus Carlsen',
+      body: "Current world champ",
+      imageUrl: "https://i.guim.co.uk/img/media/ad35976aca03db7b6b355111a3dd4e3db687d724/0_136_2048_1229/master/2048.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=c2b38f880baf6f2edba26b98fdf4420c",
+      type: "GM"
+    };
   }
 
 }
