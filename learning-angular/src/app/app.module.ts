fb2fb8ac804f93f4254073e6aca9e4cf5bd206ba
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-26 16:03:51 -0400

    Added data binding component for examples

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { TemplateSyntaxComponent } from './template-syntax/template-syntax.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
-    TemplateSyntaxComponent
+    TemplateSyntaxComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule
