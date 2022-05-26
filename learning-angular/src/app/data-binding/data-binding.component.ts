e4b7dafc6cce85c1714e3dbd88763661c9a245c2
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-26 16:14:22 -0400

    Added one way binding and event binding examples

--- learning-angular/src/app/data-binding/data-binding.component.ts
+++ learning-angular/src/app/data-binding/data-binding.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  type: string = "Chess";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  clickEvent(): void {
+    console.log("Type value is: ", this.type);
+  }
 }
