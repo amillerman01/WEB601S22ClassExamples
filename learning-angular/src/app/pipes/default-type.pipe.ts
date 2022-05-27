bf58d21957294d9badff6c44a57b946d5aac8640
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-05-27 14:57:40 -0400

    Wrote out the defaulttype pipe using if statements instead

--- learning-angular/src/app/pipes/default-type.pipe.ts
+++ learning-angular/src/app/pipes/default-type.pipe.ts
@@ -8,6 +8,18 @@ export class DefaultTypePipe implements PipeTransform {
 
   transform(contentItem: Content, defaultValue?: string): string {
     return contentItem.type || defaultValue || "IM";
+
+    // A way to visualize what's happening above is written below
+
+    // if (contentItem.type) {
+    //   return contentItem.type;
+    // }
+    // else if (defaultValue) {
+    //   return defaultValue;
+    // }
+    // else {
+    //   return "IM";
+    // }
   }
 
 }
