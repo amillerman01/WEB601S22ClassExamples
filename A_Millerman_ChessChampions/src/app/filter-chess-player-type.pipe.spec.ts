72cae58b43683f2bf978b2fa0498326aca9f8500
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-02 14:22:48 -0400

    Added filter definition and applied to the ngFors

---
+++ A_Millerman_ChessChampions/src/app/filter-chess-player-type.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterChessPlayerTypePipe } from './filter-chess-player-type.pipe';
+
+describe('FilterChessPlayerTypePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterChessPlayerTypePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ A_Millerman_ChessChampions/src/app/filter-chess-player-type.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './models/content';
+
+@Pipe({
+  name: 'filterChessPlayerType'
+})
+export class FilterChessPlayerTypePipe implements PipeTransform {
+
+  transform(listOfChessPlayers: Content[], typeToFilterBy?: string): Content[] {
+    return listOfChessPlayers.filter(chessPlayer => {
+      // Write the code that decides if we keep the chess player or not
+      if (typeToFilterBy) {
+        // only keep chess players that have their type set to be the same value as typeToFilterBy
+
+        // Can do the long way with an if statement
+        // if (typeToFilterBy === chessPlayer.type) {
+        //   return true;
+        // }
+        // else {
+        //   return false;
+        // }
+
+        // can return just the comparison directly
+        return typeToFilterBy === chessPlayer.type;
+      }
+      return !chessPlayer.type;
+    });
+  }
+
+}
