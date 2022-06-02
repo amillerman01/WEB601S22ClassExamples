afdfbd05df02e2fcd9c43243f75f18bc05d56638
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-02 16:54:11 -0400

    Completed advanced attribute directive example

--- learning-angular/src/app/directives/highlighter.directive.ts
+++ learning-angular/src/app/directives/highlighter.directive.ts
@@ -1,18 +1,41 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
 })
-export class HighlighterDirective implements OnInit {
+export class HighlighterDirective {
   @Input() colour?: string;
-  constructor(private el: ElementRef) {
+  private isHighlighted: boolean = false;
+  private initialColour: string;
+  private initialTextColor: string;
+  @HostBinding('style.backgroundColor')
+  get backgroundColour() {
+    return this.isHighlighted ? this.colour || "green" :
+      this.initialColour;
+  }
+  @HostBinding('style.color')
+  get textColour() {
+    return this.isHighlighted ? this.checkLuminance(this.colour || "green") :
+      this.checkLuminance(this.initialColour);
+  }
+  @HostBinding('attr.title')
+  get setTitleText() {
+    // I can set the title attribute on elements based on a condition too
+    return "";
   }
 
-  ngOnInit(): void {
-    this.el.nativeElement.style.backgroundColor = this.colour || "green";
+  constructor(private elm: ElementRef) {
+    this.initialColour = this.elm.nativeElement.style.backgroundColor;
+    this.initialTextColor = this.elm.nativeElement.style.color;
+    // console.log("Value of initialColor is ", this.initialColour)
   }
-  // changeColor(): void {
-  //   this.el
-  // }
 
+  @HostListener('click') onClick() {
+    this.isHighlighted = !this.isHighlighted;
+  }
+
+  checkLuminance(color: string): string {
+    // run luminance check on the color to return either black or white for the text color for maximum contrast
+    return this.isHighlighted ? "white" : "black";
+  }
 }
