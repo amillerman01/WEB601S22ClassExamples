4a45e77006f1a66b5110b4251e502bd0d4d51a83
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-09 15:31:14 -0400

    Alternative solution for the bonus on tags and type
    Missing click event for title, need to solve the colour being passed to css somehow

--- A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
+++ A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
@@ -4,7 +4,10 @@ import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angula
   selector: '[appHighlightImportantData]'
 })
 export class HighlightImportantDataDirective {
-  private isHighlighted: boolean = false;
+  private isTitleApplied: boolean = false;
+
+  private isTypeApplied: boolean = false;
+  private isTagApplied: boolean = false;
   private initialColourOfTagText: string;
 
   @Input() colour: string = '';
@@ -12,41 +15,42 @@ export class HighlightImportantDataDirective {
   @Input() highlightType?: "type" | "title" | "tag" | "author";
   // the next step would be to make an ENUM of these 3 types, so we can use that everywhere instead
   @HostBinding('style.backgroundColor') get textColour() {
-    return this.isHighlighted ? this.colour : 'transparent';
+    return this.isTitleApplied ? this.colour : 'transparent';
   }
+  @HostBinding('class.typeBorder') get typeBorder() { return this.isTypeApplied; }
+  @HostBinding('class.titleBackground') get titleBackground() { return this.isTitleApplied; }
+  @HostBinding('class.tagFontColor') get tagFontColor() { return this.isTagApplied; }
+
   constructor(private elm: ElementRef) {
     this.initialColourOfTagText = this.elm.nativeElement.style.color;
   }
 
   // hover over type or tags
   @HostListener("mouseover") onMouseOver() {
-    if (this.highlightType === "type" || this.highlightType === "author") {
-      // set type background color
-      this.elm.nativeElement.style.border = "4px solid #0000ff";
-    }
-
-    if (this.highlightType === "tag" || this.highlightType === "author") {
-      // set tag font color
-      this.elm.nativeElement.style.color = 'darkred';
-    }
+    this.hoverAffect(true);
   }
   @HostListener("mouseout") onMouseOut() {
+    this.hoverAffect(false);
+  }
+
+  private hoverAffect(newState: boolean) {
     if (this.highlightType === "type" || this.highlightType === "author") {
       // reset type background color
-      this.elm.nativeElement.style.border = "none"
+      this.isTypeApplied = newState;
+      // this.elm.nativeElement.style.border = newState ? "4px solid #0000ff" : "none";
     }
 
     if (this.highlightType === "tag" || this.highlightType === "author") {
-
       // reset tag font color back to original value
-      this.elm.nativeElement.style.color = this.initialColourOfTagText;
+      this.isTagApplied = newState;
+      // this.elm.nativeElement.style.color = newState ? 'darkred' : this.initialColourOfTagText;
     }
   }
 
   // click on title
   @HostListener("click") onClick() {
     if (this.highlightType === 'title' || this.highlightType === "author") {
-      this.isHighlighted = !this.isHighlighted;
+      this.isTitleApplied = !this.isTitleApplied;
     }
   }
 
