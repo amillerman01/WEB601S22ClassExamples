f41d717d6055c0fc4557fe7a6dad8e32cb9f1b22
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-09 15:00:28 -0400

    Restricted the functionality to each tag

--- A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
+++ A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
@@ -8,6 +8,9 @@ export class HighlightImportantDataDirective {
   private initialColourOfTagText: string;
 
   @Input() colour: string = '';
+  // highlightType can only be the values: "type", "title", "tags" or undefined
+  @Input() highlightType?: "type" | "title" | "tag";
+  // the next step would be to make an ENUM of these 3 types, so we can use that everywhere instead
   @HostBinding('style.backgroundColor') get textColour() {
     return this.isHighlighted ? this.colour : 'transparent';
   }
@@ -17,23 +20,34 @@ export class HighlightImportantDataDirective {
 
   // hover over type or tags
   @HostListener("mouseover") onMouseOver() {
-    // set type background color
-    this.elm.nativeElement.style.border = "4px solid #0000ff";
+    if (this.highlightType === "type") {
+      // set type background color
+      this.elm.nativeElement.style.border = "4px solid #0000ff";
+    }
 
-    // set tag font color
-    this.elm.nativeElement.style.color = 'darkred';
+    if (this.highlightType === "tag") {
+      // set tag font color
+      this.elm.nativeElement.style.color = 'darkred';
+    }
   }
   @HostListener("mouseout") onMouseOut() {
-    // reset type background color
-    this.elm.nativeElement.style.border = "none"
+    if (this.highlightType === "type") {
+      // reset type background color
+      this.elm.nativeElement.style.border = "none"
+    }
 
-    // reset tag font color back to original value
-    this.elm.nativeElement.style.color = this.initialColourOfTagText;
+    if (this.highlightType === "tag") {
+
+      // reset tag font color back to original value
+      this.elm.nativeElement.style.color = this.initialColourOfTagText;
+    }
   }
 
   // click on title
   @HostListener("click") onClick() {
-    this.isHighlighted = !this.isHighlighted;
+    if (this.highlightType === 'title') {
+      this.isHighlighted = !this.isHighlighted;
+    }
   }
 
 }
