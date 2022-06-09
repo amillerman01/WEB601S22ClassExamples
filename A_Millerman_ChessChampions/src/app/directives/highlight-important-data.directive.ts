d72c197486084478610c2b6d36dcc50efd822b71
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-09 14:37:54 -0400

    Added a hover affect to the type

--- A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
+++ A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightImportantData]'
 })
 export class HighlightImportantDataDirective {
 
-  constructor() { }
-
+  constructor(private elm: ElementRef) { }
+  @HostListener("mouseover") onMouseOver() {
+    this.elm.nativeElement.style.border = "4px solid #0000ff"
+  }
+  @HostListener("mouseout") onMouseOut() {
+    this.elm.nativeElement.style.border = "none"
+  }
 }
