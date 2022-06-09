2ce56e64b8f3b1d98fe39364488746276019c6f1
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-09 14:46:24 -0400

    Added the click event

--- A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
+++ A_Millerman_ChessChampions/src/app/directives/highlight-important-data.directive.ts
@@ -4,12 +4,24 @@ import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angula
   selector: '[appHighlightImportantData]'
 })
 export class HighlightImportantDataDirective {
-
+  private isHighlighted: boolean = false;
+  @Input() colour: string = '';
+  @HostBinding('style.backgroundColor') get textColour() {
+    return this.isHighlighted ? this.colour : 'transparent';
+  }
   constructor(private elm: ElementRef) { }
+
+  // hover over type
   @HostListener("mouseover") onMouseOver() {
     this.elm.nativeElement.style.border = "4px solid #0000ff"
   }
   @HostListener("mouseout") onMouseOut() {
     this.elm.nativeElement.style.border = "none"
   }
+
+  // click on title
+  @HostListener("click") onClick() {
+    this.isHighlighted = !this.isHighlighted;
+  }
+
 }
