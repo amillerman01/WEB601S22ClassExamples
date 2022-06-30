4fb8301ad5695bd04a41f630308e1e36e2394d7c
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-30 14:38:36 -0400

    Added routing for new components

--- learning-angular/src/app/app-routing.module.ts
+++ learning-angular/src/app/app-routing.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentListComponent } from './content-list/content-list.component';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
+import { InvalidLinkComponent } from './invalid-link/invalid-link.component';
 
 const routes: Routes = [
   {
+    path: "",
+    redirectTo: "/content",
+    pathMatch: "full"
+  },
+  {
     path: "content",
     component: ContentListComponent,
+  },
+  {
+    path: "content/:id",
+    component: ContentDetailComponent
+  },
+  {
+    path: "**",
+    component: InvalidLinkComponent
   }
-
 ];
 
 @NgModule({
