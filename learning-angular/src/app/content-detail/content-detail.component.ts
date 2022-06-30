91d4eaf1608eb0e0009db15b8c327d62df44f30a
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-06-30 16:05:29 -0400

    Added error checking if the user visits the content detail of an invalid id number

--- learning-angular/src/app/content-detail/content-detail.component.ts
+++ learning-angular/src/app/content-detail/content-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Content } from '../content';
 import { ChessPlayerTitle } from '../enums/chess-player-title';
 import { ContentService } from '../services/content.service';
@@ -14,14 +14,26 @@ export class ContentDetailComponent implements OnInit {
   contentItem?: Content;
   ChessPlayerTitleENUMValues = ChessPlayerTitle;
 
-  constructor(private route: ActivatedRoute,
+  constructor(private router: Router,
+    private route: ActivatedRoute,
     private contentService: ContentService) {
   }
 
   ngOnInit() {
-    this.id = Number(this.route.snapshot.paramMap.get('id'));
-    this.contentService.getContentItem(this.id).subscribe(singleItem => {
-      this.contentItem = singleItem;
-    })
+    // if a detail links to another detail, OR the user can get to a detail from a detail, we can't use the snapshot
+    // this.id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.route.paramMap.subscribe(params => {
+      this.id = +(params.get('id') ?? 0); // uses the + unary operator
+
+      this.contentService.getContentItem(this.id).subscribe(singleItem => {
+        if (singleItem) {
+          this.contentItem = singleItem;
+        }
+        else {
+          this.router.navigate(['/contentNotFound']);
+        }
+      });
+    });
   }
 }
