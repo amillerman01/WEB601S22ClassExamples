6d12c95ef6572290f509098410d610e758a12f15
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-07-07 14:42:26 -0400

    Updated the content in the detail component
    SCSS to come later

--- A_Millerman_ChessChampions/src/app/content-detail/content-detail.component.ts
+++ A_Millerman_ChessChampions/src/app/content-detail/content-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Content } from '../models/content';
+import { ChessChampionService } from '../services/chess-champion.service';
 
 @Component({
   selector: 'app-content-detail',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentDetailComponent implements OnInit {
 
-  constructor() { }
+  id?: number;
+  individualChessPlayer?: Content;
+
+  constructor(private route: ActivatedRoute,
+    private chessChampionService: ChessChampionService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.id = Number(params.get("id") ?? 0);
+
+      this.chessChampionService.getContentItem(this.id)
+        .subscribe((individualChessPlayer) => {
+          this.individualChessPlayer = individualChessPlayer;
+        });
+    });
   }
 
 }
